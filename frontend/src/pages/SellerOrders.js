935723bf892a71906761485b54f0ac65c2954dc5
Uden Maman Suteja <desananjung1@gmail.com>
2026-07-03 10:49:02 +0700

    Disable ESLint for Vercel build

--- frontend/src/pages/SellerOrders.js
+++ frontend/src/pages/SellerOrders.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 import api from '../services/api';
 import { ClockIcon, CheckCircleIcon, TruckIcon, XCircleIcon } from '@heroicons/react/24/outline';
 
